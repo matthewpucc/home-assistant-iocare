d62f90cf7f6a2a298acf0db3afcfa6ef9e73701e
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2022-02-24 18:39:19 -0500

    add preset_mode and platform constants

--- custom_components/coway/const.py
+++ custom_components/coway/const.py
@@ -1,6 +1,14 @@
 """My platform constants"""
+from homeassistant.const import Platform
 
 DOMAIN = "coway"
+
+PLATFORMS = [
+    Platform.FAN,
+    Platform.SELECT,
+    Platform.SENSOR,
+    Platform.SWITCH,
+]
 
 IOCARE_FAN_OFF = "0"
 IOCARE_FAN_LOW = "1"
@@ -12,3 +20,6 @@ IOCARE_TIMER_1H = "60"
 IOCARE_TIMER_2H = "120"
 IOCARE_TIMER_4H = "240"
 IOCARE_TIMER_8H = "480"
+
+PRESET_MODE_AUTO = "Auto"
+PRESET_MODE_NIGHT = "Night"
