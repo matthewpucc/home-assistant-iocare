89887b313bae2b3b052a81fd0635deb43b8bf9d9
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2022-10-02 00:37:26 -0400

    Improved conformation to PEP8

--- custom_components/coway/switch.py
+++ custom_components/coway/switch.py
@@ -1,12 +1,11 @@
-""" Switch platform for Coway integration. """
+"""Switch platform for Coway integration."""
 from __future__ import annotations
 
 from typing import Any
 
-
 from cowayaio.purifier_model import CowayPurifier
-from homeassistant.components.switch import SwitchEntity
 
+from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -15,15 +14,15 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN, LOGGER
 from .coordinator import CowayDataUpdateCoordinator
 
+
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """ Set Up Coway Switch Entities. """
+    """Set Up Coway Switch Entities."""
 
     coordinator: CowayDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
     switches = []
-
 
     for purifier_id, purifier_data in coordinator.data.purifiers.items():
             switches.extend((
@@ -34,7 +33,7 @@ async def async_setup_entry(
 
 
 class PurifierLight(CoordinatorEntity, SwitchEntity):
-    """ Representation of purifier light switch. """
+    """Representation of purifier light switch."""
 
     def __init__(self, coordinator, purifier_id):
         super().__init__(coordinator)
@@ -42,13 +41,13 @@ class PurifierLight(CoordinatorEntity, SwitchEntity):
 
     @property
     def purifier_data(self) -> CowayPurifier:
-        """ Handle coordinator purifier data. """
+        """Handle coordinator purifier data."""
 
         return self.coordinator.data.purifiers[self.purifier_id]
 
     @property
     def device_info(self) -> dict[str, Any]:
-        """ Return device registry information for this entity. """
+        """Return device registry information for this entity."""
 
         return {
             "identifiers": {(DOMAIN, self.purifier_data.device_attr['device_id'])},
@@ -59,31 +58,31 @@ class PurifierLight(CoordinatorEntity, SwitchEntity):
 
     @property
     def unique_id(self) -> str:
-        """ Sets unique ID for this entity. """
+        """Sets unique ID for this entity."""
 
         return self.purifier_data.device_attr['device_id'] + '_light'
 
     @property
     def name(self) -> str:
-        """ Return name of the entity. """
+        """Return name of the entity."""
 
         return "Light"
 
     @property
     def has_entity_name(self) -> bool:
-        """ Indicate that entity has name defined. """
+        """Indicate that entity has name defined."""
 
         return True
 
     @property
     def icon(self) -> str:
-        """ Set purifier switch icon to lightbulb. """
+        """Set purifier switch icon to lightbulb."""
 
         return 'mdi:lightbulb'
 
     @property
     def is_on(self) -> bool:
-        """ Return true if light AND purifier are on. """
+        """Return true if light AND purifier are on."""
 
         if self.purifier_data.is_on and self.purifier_data.light_on:
             return True
@@ -91,7 +90,7 @@ class PurifierLight(CoordinatorEntity, SwitchEntity):
             return False
 
     async def async_turn_on(self, **kwargs) -> None:
-        """ Turn the switch on. """
+        """Turn the switch on."""
 
         if self.purifier_data.is_on:
             await self.coordinator.client.async_set_light(self.purifier_data.device_attr, True)
@@ -99,11 +98,12 @@ class PurifierLight(CoordinatorEntity, SwitchEntity):
         else:
             LOGGER.error(f'{self.purifier_data.device_attr["name"]} light can only be controlled when the purifier is On.')
             self.purifier_data.light_on = False
+
         self.async_write_ha_state()
         await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs) -> None:
-        """ Turn the switch off. """
+        """Turn the switch off."""
 
         if self.purifier_data.is_on:
             await self.coordinator.client.async_set_light(self.purifier_data.device_attr, False)
@@ -111,12 +111,13 @@ class PurifierLight(CoordinatorEntity, SwitchEntity):
         else:
             LOGGER.error(f'{self.purifier_data.device_attr["name"]} light can only be controlled when the purifier is On.')
             self.purifier_data.light_on = False
+
         self.async_write_ha_state()
         await self.coordinator.async_request_refresh()
 
     @property
     def available(self) -> bool:
-        """ Return true if purifier is connected to Coway servers. """
+        """Return true if purifier is connected to Coway servers."""
 
         if self.purifier_data.network_status:
             return True
